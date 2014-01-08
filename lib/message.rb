d53c94caffe37fd5322b5e4d8c3ed04666c2bc95
Derek Rockwell <derek.a.rockwell@gmail.com>
2014-01-08 12:58:06 -0600

    add closing delimiter to to_hl7 w/ optional flag

--- lib/message.rb
+++ lib/message.rb
@@ -168,7 +168,7 @@ class HL7::Message
 
   # provide a HL7 spec version of the message
   def to_hl7(terminating_delimeter = true)
-    @segments.collect { |s| s if s.to_s.length > 0 }.join( @delimiter.segment ) + terminating_delimeter ? @delimiter.segment : ""
+    @segments.collect { |s| s if s.to_s.length > 0 }.join( @delimiter.segment ) + (terminating_delimeter ? @delimiter.segment : "")
   end
 
   # provide the HL7 spec version of the message wrapped in MLLP
